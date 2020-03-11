e90fc017ee3e9bd04e6492ab3ef84ffe18db237f
GustavoDell <gustavo.dell@outlook.com>
2020-03-11 15:56:51 -0300

    3.GameOver com eventos

---
+++ alura-typer/public/js/main.js
@@ -0,0 +1,30 @@
+var frase = $(".frase").text();//text() retorna o conteudo que se encontra dentro da tag html
+var numPalavras = frase.split(" ").length;//split() dividi o conteudo em que esta dentro da variavel em um array
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras)
+
+var campo = $(".campo-digitacao")
+
+campo.on("input", function(){//.on() fuca sempre ouvindo a função possibilitanto chamar mais de uam vez
+
+    var conteudo = campo.val()//Função .val() da acesso ao valor do input do usuários
+
+    var qtdPalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdPalavras);
+    
+    var qtdCaracteres = conteudo.length
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus",function(){ //focus pega qualquer interação com o campo //.one() chama a função uma unica vez diferente da .on()
+    var cronometroID = setInterval(function(){
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if(tempoRestante < 1){
+            campo.attr("disabled", true)//.attr() serve para pegar os atributos do html ou então altera-lós
+            clearInterval(cronometroID);//clearInterval da stop no setInterval()
+        }
+
+    },1000);//Função pura do javascript que executa uma ação de tanto em tanto tempo 
+});
